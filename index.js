5fff78860cc83ade83a2ccc39f5ff8f0da0292d0
Nguyen Xuan Huy <huynx42@due.edu.vn>
2022-03-24 17:14:58 +0700

    update buoi 15

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ app.set("views","./views");
 app.use(express.static(__dirname + '/views'));
  
 app.get('/', (req,res) => {
-    res.render("index");
+    let posts = [
+        {
+            title: 'Man must explore, and this is exploration at its greatest',
+            subtitle: 'Problems look mighty small from 150 miles up',
+            author: 'Thinh Le',
+            postDate: 'September 24,2021',
+            url: 'https://www.24h.com.vn/'
+        },
+        {
+            title: 'Man2 must explore, and this is exploration at its greatest',
+            subtitle: 'Problems look mighty small from 150 miles up',
+            author: 'Thinh Le',
+            postDate: 'September 24,2021',
+            url: 'https://bongdaplus.vn/'
+        }
+    ]
+    res.render("index", {posts: posts});
 })
 
 app.get('/contact', (req,res) => {
